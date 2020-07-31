fe86598340886845a2a37d5572799e72bf503235
MarcusDeLaville <57030807+MarcusDeLaville@users.noreply.github.com>
2020-07-31 21:12:10 +0300

    init unity project

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _enemies;
+    [SerializeField] private GameResults _gameResults;
+
+    private void Start()
+    {
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    private void Update()
+    {
+        foreach (var enemy in _enemies)
+        {
+            if (enemy == null)
+                continue;
+
+            if (Vector3.Distance(gameObject.transform.position, enemy.gameObject.transform.position) < 0.2f)
+            {
+                Destroy(enemy);
+                _gameResults.CheakEnemys();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private float _horizontalInput;
+    private float _verticalInput;  
+
+    private void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(_horizontalInput, _verticalInput, 0) * _speed * Time.deltaTime);
+    }
+
+    
+}
